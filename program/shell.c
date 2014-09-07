641eb10aaf8eee32c467c20a33b45b8323c80d15
Georgios Koloventzos <gk2409@columbia.edu>
2014-09-07 08:02:12 -0400

    adding check for null path

--- program/shell.c
+++ program/shell.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 void parse(char *buf, char **args);
-void execute(char **args);
+void execute(char **args, char ** path);
+
+
 int main()
 { 
-	char buf[1024]; char *args[64];
+	char buf[2048]; char *args[128];
+	char **path = NULL;
 	for (;;) {
-		printf("Command: ");
+		printf("$");
 		if (fgets(buf, sizeof(buf), stdin) == NULL) {
 			printf("\n"); exit(0); }
 			parse(buf, args);
-			execute(args); 
+			execute(args, path); 
 	}
 	return 0;
 }
+
 void parse(char *buf, char **args)
 { 
 	while (*buf != '\0') {
 		while ((*buf == ' ') || (*buf == '\t') || (*buf == '\n'))
 			*buf++ = '\0';
 		*args++ = buf;
-		while ((*buf != '\0') && (*buf != ' ') && (*buf != '\t') && (*buf != '\n'))
+		while ((*buf != '\0') && (*buf != ' ') && (*buf != '\t') && \
+			(*buf != '\n'))
 			buf++; }
 		*--args = NULL; 
 }
-void execute(char **args)
+
+void execute(char **args, char **path)
 { 
 	int pid, status;
+	if (path == NULL){
+		int n = -1;
+		n = strcmp(args[0],"path");
+		n = strcmp(args[0],"exit");
+		if ( n != 0){
+			printf("Error: path is null\nPlease add some path fir\
+st.\n");
+			return;
+		}
+	}
 	if ((pid = fork()) < 0) {
-		perror("fork"); exit(1); 
+		perror("fork");
 	}
 	if (pid == 0) {
-		execvp(*args, args); perror(*args); exit(1); 
+		execvp(*args, args);
+		perror(*args);
 	}
 	if (wait(&status) != pid) {
-		perror("wait"); exit(1); 
+		perror("wait");
 	} 
 }
