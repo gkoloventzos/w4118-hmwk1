be0203f370c52276915f7d61080cf11770af75e5
Georgios Koloventzos <gk2409@columbia.edu>
2014-09-07 11:49:34 -0400

    adding path with proper pointers

--- program/shell.c
+++ program/shell.c
@@ -3,12 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 void parse(char *buf, char **args);
-void execute(char **args, char ** path);
-void mypath(char **args, char ** path);
+void execute(char **args, char *** path);
+void mypath(char **args, char *** path);
 
 int main()
-{ 
+{
 	char buf[2048]; char *args[128];
 	char **path = NULL;
 	for (;;) {
@@ -16,7 +17,7 @@ int main()
 		if (fgets(buf, sizeof(buf), stdin) == NULL) {
 			printf("\n"); exit(0); }
 			parse(buf, args);
-			execute(args, path); 
+			execute(args, &path);
 	}
 	return 0;
 }
@@ -33,7 +34,7 @@ void parse(char *buf, char **args)
 		*--args = NULL; 
 }
 
-void execute(char **args, char **path)
+void execute(char **args, char ***path)
 { 
 	int pid, status;
 	int n = -1;
@@ -49,6 +50,7 @@ st.\n");
 	}
 	if (l == 0)
 		exit(0);
+	}
 	if (n == 0){
 		mypath(args, path);
 		return;
@@ -65,29 +67,69 @@ st.\n");
 	} 
 }
 
-void mypath(char **args, char **path){
+void mypath(char **args, char ***path){
 
 	char *path_new;
 	char *ppath = NULL;
-	int i;
+	int i,z;
 	size_t len;
 	if (args[1] == NULL){
-		if (path == NULL)
-			return;
-		path_new=path[0];
+		if ((*path) == NULL){
+			return;}
+		path_new=(*path)[0];
+			printf("here\n");
+		while (path_new != NULL){
+			if (*ppath == NULL){
+				size_t d = strlen(path_new);
+				ppath = malloc(d+2);
+				strcpy(ppath,path_new);
+				ppath[d+1] = ":";
+				ppath[d+2] = "\0";
+			}
+			else{
+				z = strlen(ppath);
+				ppath = realloc(ppath,z+strlen(path_new)+2);
+			}
+			if (ppath == NULL){
+				printf("Error in locating space for print path.\n");
+				return;
+			}
+			
+		}
 		for(i=0; path_new != NULL; i++){
-		printf("here\n");
-			path_new = path[i];
+			printf("here\n");
+			path_new = (*path)[i];
 			ppath = realloc(ppath,strlen(path_new)+1);
 			len = strlen(ppath);
 			ppath[len]=":";
 		}
 		if (ppath != NULL){
+			printf("here\n");
 			len = strlen(ppath);
                 	ppath[len]="\0";
 		}
-		printf("before print\n");
+			printf("there\n");
 		printf("PATH=%s",ppath);
+		return;
+	}
+	if (args[1] == NULL || args[2] == NULL){
+		printf("Use of path: path [+|- absolute path]\n");
+		return;
+	}	
+	if (strncmp(args[1],"-",1) == 0){
+		return;
+	}
+	if (strncmp(args[1],"+",1) == 0){
+		if ((*path) == NULL){
+			(* path) = (char **)calloc(1,sizeof(char*));
+			if ((*path) == NULL){
+				printf("Error in locating space for new path!\n");
+			}
+			(*path)[0] = strdup(args[2]);
+			if ((*path)[0] == NULL){
+				printf("Error in locating space for new path!\n");
+			}
+		}
 		return;
 	}
 	
