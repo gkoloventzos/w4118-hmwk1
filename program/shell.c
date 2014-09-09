d3ddc961bb1ada03fc349835219dc028d5842186
Georgios Koloventzos <gk2409@columbia.edu>
2014-09-08 21:05:23 -0400

    continuing adding path functionality

--- program/shell.c
+++ program/shell.c
@@ -5,19 +5,21 @@
 #include <sys/wait.h>
 
 void parse(char *buf, char **args);
-void execute(char **args, char *** path);
-void mypath(char **args, char *** path);
+void execute(char **args, char *** path, int *len);
+void mypath(char **args, char *** path, int *len);
 
 int main()
 {
 	char buf[2048]; char *args[128];
+    bzero(buf,1024);
 	char **path = NULL;
+    int llength = 0;
 	for (;;) {
 		printf("$ ");
 		if (fgets(buf, sizeof(buf), stdin) == NULL) {
 			printf("\n"); exit(0); }
 			parse(buf, args);
-			execute(args, &path);
+			execute(args, &path,&llength);
 	}
 	return 0;
 }
@@ -34,11 +36,14 @@ void parse(char *buf, char **args)
 		*--args = NULL; 
 }
 
-void execute(char **args, char ***path)
+void execute(char **args, char ***path, int *ll)
 { 
 	int pid, status;
 	int n = -1;
 	int l = -1;
+    if (args[0] == NULL){
+        return;
+    }
 	n = strcmp(args[0],"path");
 	l = strcmp(args[0],"exit");
 	if ((*path) == NULL){
@@ -53,7 +58,7 @@ st.\n");
 		exit(0);
 	}
 	if (n == 0){
-		mypath(args, path);
+		mypath(args, path, ll);
 		return;
 	}
 	if ((pid = fork()) < 0) {
@@ -68,49 +73,66 @@ st.\n");
 	} 
 }
 
-void mypath(char **args, char ***path){
+void mypath(char **args, char ***path, int *leng){
 
 	char *path_new;
 	char *ppath = NULL;
-	int i,z;
+	int i,z,path_length;
 	size_t len;
 	if (args[1] == NULL){
 		if ((*path) == NULL){
 			return;}
-		path_new=(*path)[0];
-			printf("here\n");
-		while (path_new != NULL){
-			if (*ppath == NULL){
+        path_length = *leng;
+        path_length--;
+		printf("here\n");
+        i = 0;
+		do{
+            printf("i: %d\n",i);
+            path_new = (*path)[i];
+			if (ppath == NULL){
 				size_t d = strlen(path_new);
-				ppath = malloc(d+2);
+				ppath = (char*) malloc((d+2)*sizeof(char));
+			    if (ppath == NULL){
+				    printf("Error in locating space for print path.\n");
+				    return;
+			    }
 				strcpy(ppath,path_new);
-				ppath[d+1] = ":";
-				ppath[d+2] = "\0";
+				ppath[d+1] = ':';
+				ppath[d+2] = '\0';
+    
 			}
 			else{
 				z = strlen(ppath);
-				ppath = realloc(ppath,z+strlen(path_new)+2);
+            printf("z: %d\n",z);
+                z = strlen(path_new);    
+				ppath = realloc(ppath,(z+strlen(path_new)+2)*sizeof(char));
+            printf("z: %d\n",z);
+			    if (ppath == NULL){
+				    printf("Error in locating space for print path.\n");
+				    return;
+			    }
+                strcat(ppath,":");
+            printf("z: %d\n",z);
+                strcat(ppath,path_new);
+            printf("z: %d\n",z);
+                
+		        printf("PATH=%s\n",ppath);
 			}
-			if (ppath == NULL){
-				printf("Error in locating space for print path.\n");
-				return;
-			}
-			
-		}
-		for(i=0; path_new != NULL; i++){
+			i++;
+		}while (i <= path_length);
+		/*for(i=0; path_new != NULL; i++){
 			printf("here\n");
 			path_new = (*path)[i];
 			ppath = realloc(ppath,strlen(path_new)+1);
 			len = strlen(ppath);
-			ppath[len]=":";
-		}
+			ppath[len] = ':';
+		}*/
 		if (ppath != NULL){
 			printf("here\n");
 			len = strlen(ppath);
-                	ppath[len]="\0";
+                	ppath[len] = '\0';
 		}
-			printf("there\n");
-		printf("PATH=%s",ppath);
+		printf("PATH=%s\n",ppath);
 		return;
 	}
 	if (args[1] == NULL || args[2] == NULL){
@@ -130,7 +152,14 @@ void mypath(char **args, char ***path){
 			if ((*path)[0] == NULL){
 				printf("Error in locating space for new path!\n");
 			}
+            (*leng)++;
+            printf("len: %d\n",*leng);
+            return;
 		}
+        (* path) = (char **)realloc((* path),((*leng)+1)*sizeof(char*));
+        (* path)[(*leng)+1] = strdup(args[2]);
+        (*leng)++;
+            printf("len: %d\n",*leng);
 		return;
 	}
 	
