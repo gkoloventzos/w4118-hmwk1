3662b3d562127c7cf80cc1a06333c1415ffbb1f0
Georgios Koloventzos <gk2409@columbia.edu>
2014-09-12 22:16:35 -0400

    adding shell.h

---
+++ shell/shell.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void parse(char *buf, char **args);
+void execute(char ***args, char ***path, int *len, int pipes);
+void mypath(char **args, char ***path, int *len);
+void path_print(char **path, int len, int in_line);
+pid_t my_fork(char *cmd, char **args, int **pipes, int pipe_num, int allpipes);
